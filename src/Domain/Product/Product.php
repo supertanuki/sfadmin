fc0fd3916aeb4dfe650f1f4a07789a466cc55c54
Richard Hanna <richard.hanna@elao.com>
2017-12-30 21:56:36 +0100

    Add product image field

--- src/Domain/Product/Product.php
+++ src/Domain/Product/Product.php
@@ -2,6 +2,9 @@
 
 namespace App\Domain\Product;
 
+use Symfony\Component\HttpFoundation\File\File;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+
 class Product
 {
     /** @var null|int */
@@ -16,6 +19,15 @@ class Product
     /** @var \DateTimeInterface */
     private $createdAt;
 
+    /** @var \DateTimeInterface */
+    private $updatedAt;
+
+    /** @var string */
+    private $image;
+
+    /** @var null|File */
+    private $imageFile;
+
     public function __construct(
         string $label,
         int $unitPrice,
@@ -24,6 +36,7 @@ class Product
         $this->label = $label;
         $this->unitPrice = $unitPrice;
         $this->createdAt = $createdAt;
+        $this->updatedAt = $createdAt;
     }
 
     public function getId(): ?int
@@ -46,6 +59,11 @@ class Product
         return $this->createdAt;
     }
 
+    public function getUpdatedAt(): \DateTimeInterface
+    {
+        return $this->updatedAt;
+    }
+
     public function setLabel(string $label): void
     {
         $this->label = $label;
@@ -59,5 +77,45 @@ class Product
     public function setCreatedAt(\DateTimeInterface $createdAt): void
     {
         $this->createdAt = $createdAt;
+    }
+
+    public function setUpdatedAt(\DateTimeInterface $updatedAt): void
+    {
+        $this->updatedAt = $updatedAt;
+    }
+
+    /**
+     * If manually uploading a file (i.e. not using Symfony Form) ensure an instance
+     * of 'UploadedFile' is injected into this setter to trigger the  update. If this
+     * bundle's configuration parameter 'inject_on_load' is set to 'true' this setter
+     * must be able to accept an instance of 'File' as the bundle will inject one here
+     * during Doctrine hydration.
+     *
+     * @param File|UploadedFile $image
+     */
+    public function setImageFile(?File $image = null): void
+    {
+        $this->imageFile = $image;
+
+        if (null !== $image) {
+            // It is required that at least one field changes if you are using doctrine
+            // otherwise the event listeners won't be called and the file is lost
+            $this->updatedAt = new \DateTimeImmutable();
+        }
+    }
+
+    public function getImageFile(): ?File
+    {
+        return $this->imageFile;
+    }
+
+    public function setImage(?string $image): void
+    {
+        $this->image = $image;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
     }
 }
